0e080ab336ab6a0e6f9a5f6d64b13d58fd7394ad
Hayg Astourian <hayg@meraki.com>
2015-08-29 00:05:38 -0700

    Add node implementation of a trie
    * Only supports the contains function
    * Only supports strings
    * Only depends on node's file module to laod the dictionary
    * Just runs some hard coded test cases for now

---
+++ trie.js
@@ -0,0 +1,87 @@
+var fs = require("fs");
+
+function trieNode(isWord, value, children) {
+  return {isWord: isWord, value: value, children: children || {}};
+}
+
+function triePrint(root) {
+  console.log(root.value);
+  console.log("===");
+  for (var key in root.children) {
+    triePrint(root.children[key]);
+  }
+  console.log("===");
+}
+
+function trieInsert(trie, word, index) {
+  index = index || 0;
+  if (!root) {
+    console.error("No root passed to trieInsert");
+  }
+
+  if (word.length === 0) return;
+
+  if (trie.children[word[index]]) {
+    if (index === word.length - 1) {
+      trie.children[word[index]].isWord = true;
+    } else {
+      trieInsert(trie.children[word[index]], word, index + 1);
+    }
+  } else {
+    var isWord = (word.length - 1 === index);
+    trie.children[word[index]] = trieNode(isWord, word[index]);
+    if (!isWord) {
+      trieInsert(trie.children[word[index]], word, index + 1);
+    }
+  }
+}
+
+var contains = function(trie, word, index) {
+  index = index || 0;
+  if (trie.children[word[index]]) {
+    if (index === word.length - 1) {
+      if (trie.children[word[index]].isWord) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return contains(trie.children[word[index]], word, index + 1);
+    }
+  } else {
+    return false;
+  }
+};
+
+function buildTrie(words) {
+  console.log("Beginning to build trie...");
+  var trie = trieNode();
+  var word;
+  for (var i=0; i < words.length; i++) {
+    word = words[i];
+    console.log("Inserting ", word);
+    trieInsert(trie, word);
+  }
+  console.log("Done building trie!");
+
+  return trie;
+}
+
+function isEnglish(trie, word) {
+  console.log(word, " is English: ", contains(trie, word));
+}
+
+fs.readFile('/usr/share/dict/words', 'utf8', function (err, words) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  words = words.split('\n');
+  var trie = buildTrie(words);
+
+  isEnglish(trie, "aardvark");
+  isEnglish(trie, "hayg");
+  isEnglish(trie, "pernicious");
+  isEnglish(trie, "ingenious");
+  isEnglish(trie, "gabig");
+});
